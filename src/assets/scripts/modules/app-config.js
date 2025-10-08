c24b1a2276f8ffd4b7d7d2a6adfe6d6879619018
James Barnard <jmbarnard@gmail.com>
2025-10-08 10:12:32 +0100

    Update api reference

--- src/assets/scripts/modules/app-config.js
+++ src/assets/scripts/modules/app-config.js
@@ -1,5 +1,7 @@
 const app = {
+    api: window.location.hostname.includes('localhost') ? 'https://localhost:4000' : `https://api.apptiser.io`,
     isProd: process.env.NODE_ENV === 'production',
-    server: process.env.NODE_ENV === 'production'? `https://api.apptiser.io` : `http://localhost:4000`
+    server: process.env.NODE_ENV === 'production' ? `https://api.apptiser.io` : `http://localhost:4000`
 }
-export { app }
+
+export default app;
